6d6016a160cefb98e48c662c252a955ad3540c33
Manuel Renz <man.renz@gmail.com>
2019-10-26 04:21:17 +0200

    add all data

--- print_most_used_streets.py
+++ print_most_used_streets.py
@@ -1,6 +1,11 @@
 # written by Manuel Renz
 
 import json
+import collections
+
+mapping = collections.OrderedDict()
+street_name = []
+daily_traffic = []
 
 with open('planning-sections-all-new.json') as json_file:
   data = json.load(json_file)
@@ -8,8 +13,27 @@ with open('planning-sections-all-new.json') as json_file:
   for i in range(0, len(data)-1):
     # iterate through each entry in each page
     for j in range(0, 10):
-      print(data[i]['results'][j]['details'][0]['daily_traffic'])
+      mapping[data[i]['results'][j]['name']] = data[i]['results'][j]['details'][0]['daily_traffic']
+      daily_traffic.append(data[i]['results'][j]['details'][0]['daily_traffic'])
 
   # iterate through last page
   for j in range(0, 8):
-    print(data[257]['results'][j]['details'][0]['daily_traffic'])
+    daily_traffic.append(data[257]['results'][j]['details'][0]['daily_traffic'])
+
+#data.sort(key=lambda x: x[i]['results'][j]['details'][0]['daily_traffic'], reverse = True)
+
+#mapping.sort(reverse = True)
+daily_traffic.sort(reverse = True)
+
+#for k in sorted(mapping.keys()):
+#  print(k + ", " + mapping[k])
+
+cnt = 0
+for k in sorted(mapping.items(), key=lambda item: item[1], reverse = True):
+  if cnt > 10:
+    break
+  print(k)
+  cnt += 1
+
+#for i in range(0,10):
+#  print(daily_traffic[i])
